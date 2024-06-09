f3cbbb9793861974b52f136afbf07eb572436b81
gustavcampos <gustavop.campos2004@gmail.com>
2024-06-09 04:42:59 -0300

    Adding logs to data processing

--- Sprint 5/Desafios/data-processing/data_processing.py
+++ Sprint 5/Desafios/data-processing/data_processing.py
@@ -27,21 +27,26 @@ def main():
     NR_ID = "NR_IDENTIFICACAO"
     START_DATE = "DATA_INICIO_OFERTA"
     
+    print(f"Loading data from {JSON_FILE}...")
     with open(JSON_FILE, "r") as file:
         json_dict = json.load(file)
         main_df = pd.DataFrame(json_dict["data"])
     
+    print(f"Formatting {CNPJ} attribute...")
     main_df[CNPJ] = main_df[CNPJ].apply(process_cnpj)
     
+    print(f"Formatting {NR_ID} attribute...")
     main_df[NR_ID] = main_df[NR_ID].apply(
         lambda n: (n.replace(".", ""), None)[not n]
     )
     
+    print(f"Formatting {START_DATE} attribute...")
     main_df[START_DATE] = main_df[START_DATE].apply(process_date)
     
-    print("Data processing complete!")
-    
+    print(f"Writing data as CSV to {CSV_FILE}...")
     main_df.to_csv(CSV_FILE, encoding="utf-8", index=False)
+    
+    print("Data processing complete!")
 
 if __name__ == "__main__":
     main()
