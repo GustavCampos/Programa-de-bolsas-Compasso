7c36df6920b2146ea48a9b2f519c671f23a0989a
gustavcampos <gustavop.campos2004@gmail.com>
2024-07-17 20:02:39 -0300

    glue setup finalization

--- Sprint 8/Desafios/test.py
+++ Sprint 8/Desafios/test.py
@@ -1,18 +1,19 @@
-# TODO: import sys
+import sys
 from awsglue.transforms import *
-# TODO: from awsglue.utils import getResolvedOptions
+from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
-# TODO: from awsglue.job import Job
-
-# TODO: Remove dotenv import
+from awsglue.job import Job
 
 # Custom imports ==================================================================================
-import re as regex
 import boto3
+import json
+import re as regex
 from botocore.exceptions import ClientError
 
+
 # Custom Functions ================================================================================
+# S3 key manipulation functions _______________________________________________
 def bucket_name_from_url(url: str) -> str:
     return url.split("/")[2]
 
@@ -24,33 +25,70 @@ def date_from_s3_key(obj_key: str) -> str:
     date = regex.search(rule, obj_key).group()
     return '-'.join(date.split("/")[:-1])
 
-def main():
-    # Loading Env Vars ________________________________________________________
-    load_dotenv()
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
-    S3_MOVIE_INPUT_PATH = os.getenv("S3_MOVIE_INPUT_PATH")
-    S3_SERIES_INPUT_PATH = os.getenv("S3_SERIES_INPUT_PATH")
-    S3_TARGET_PATH = os.getenv("S3_TARGET_PATH")
+# _____________________________________________________________________________
+def load_args(arg_list: list=None, file_path: str=None) -> dict:
+    if "--local" in sys.argv:
+        with open(file_path) as file:
+            return json.load(file)
+    else:
+        return getResolvedOptions(sys.argv, arg_list)
     
+# Main Function ================================================================================
+def main():
+    # Loading Job Parameters __________________________________________________
+    ARGS_LIST = [
+        "JOB_NAME",
+        "S3_MOVIE_INPUT_PATH", 
+        "S3_SERIES_INPUT_PATH", 
+        "S3_TARGET_PATH"
+    ]
+    
+    args = load_args(ARGS_LIST, "createTrustedDataLocal_parameters.json")
+    
+    # Creating Job Context ____________________________________________________
+    sc = SparkContext()
+    glueContext = GlueContext(sc)
+    spark = glueContext.spark_session
+    job = Job(glueContext)
+    job.init(args[ARGS_LIST[0]], args)
+    # # Custom Code ===============================================================================
+    
+    # df = glueContext.create_dynamic_frame.from_options(
+    #     "s3", {"paths": ["s3://gustavcampos-pb-data-lake/Raw/Local/CSV/Movies/2024/07/01/movies.csv"]},
+    #     "csv", {"withHeader": True, "separator":"|"},
+    # )
+    
+    # df.show()
+    
+    
+    
+    
+    
+    
+    
+    # Setting Constants _______________________________________________________ 
+    S3_MOVIE_INPUT_PATH = args[ARGS_LIST[1]]
+    S3_SERIES_INPUT_PATH = args[ARGS_LIST[2]]
+    S3_TARGET_PATH = args[ARGS_LIST[3]]
     BUCKET_NAME = bucket_name_from_url(S3_MOVIE_INPUT_PATH)
 
-    s3_client = boto3.client("s3",
-        aws_access_key_id=      AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=  AWS_SECRET_ACCESS_KEY,
-        aws_session_token=      AWS_SESSION_TOKEN
-    )
+    s3_client = boto3.client("s3")
 
     response = s3_client.list_objects(
         Bucket=BUCKET_NAME, 
         Prefix=s3_key_from_url(S3_MOVIE_INPUT_PATH)
     )
 
-    for obj in response["Contents"]:
-        obj_date = date_from_s3_key(obj["Key"])
+    print(response)
 
-        
+    for obj in response["Contents"]:
+        key = obj["Key"]
+        obj_date = date_from_s3_key(key)
+        print(f"Key: {key}")
+        print(f"Date: {obj_date}")
+    
+    # Custom Code End =============================================================================
+    job.commit()
         
 if __name__ == "__main__":
     main()
